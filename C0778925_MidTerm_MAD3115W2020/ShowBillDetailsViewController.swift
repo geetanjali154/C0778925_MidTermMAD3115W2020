f5672cd0943f7fc73f404308f86be17bdcd6eeb7
Geetanjali <geetanjaligupta154@gmail.com>
2020-03-07 18:24:10 -0500

    bill

--- C0778925_MidTerm_MAD3115W2020/ShowBillDetailsViewController.swift
+++ C0778925_MidTerm_MAD3115W2020/ShowBillDetailsViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ShowBillDetailsViewController: UIViewController {
 
- 
+ override func viewDidLoad() {
+      super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
     
