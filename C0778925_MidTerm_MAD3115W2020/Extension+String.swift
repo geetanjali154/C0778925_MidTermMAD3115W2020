d7ad8584ce7ddbb484f6a7e741b9aa27d86a2fc4
Geetanjali <geetanjaligupta154@gmail.com>
2020-03-07 18:16:42 -0500

    reading from plist

---
+++ C0778925_MidTerm_MAD3115W2020/Extension+String.swift
@@ -0,0 +1,23 @@
+//
+//  Extension+String.swift
+//  C0778925_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-07.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+extension String{
+    func isEmailValid(email:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+        }
+    
+    
+    func mobileValid() -> Bool{
+        let mobileNumberRegEx = "[0-9]{10}"
+        let mobileTest = NSPredicate(format: "SELF MATCHES %@", mobileNumberRegEx)
+        return mobileTest.evaluate(with: self)
+    }
+}
